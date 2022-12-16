17edc8f6e9d1e6d9080af15c9f6bb9333ab297c3
victorramos887 <v.ramos58@hotmail.com>
2022-12-16 18:45:46 -0300

    save

--- Django01/usuarios/views.py
+++ Django01/usuarios/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    # sourcery skip: remove-unnecessary-else, swap-if-else-branches
+
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+
+        if not nome.strip():
+            print('O nome não pode ficarm em branco')
+            return redirect('cadastro')
+
+        if not email.strip():
+            print('O email não pode ficar em branco')
+            return redirect('cadastro')
+
+        if senha != senha2:
+            print('a senha não é igual')
+            return redirect('cadastro')
+
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
 
 
 def login(request):
